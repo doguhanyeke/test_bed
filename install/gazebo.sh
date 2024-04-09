feea0d26fc9d3865e5b7c87919f57a94e3b28fbc
Kartik Anand Pant <kpant14@gmail.com>
2024-04-09 11:29:44 -0400

    change env name to mixed sense

--- install/gazebo.sh
+++ install/gazebo.sh
@@ -2,12 +2,11 @@
 set -e
 set -x
 
-GAZEBO_VERSION="garden"
+
+sudo apt-get update
+sudo apt-get install lsb-release wget gnupg -y
 
 sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
-	gz-${GAZEBO_VERSION}
-sudo apt install -y libignition-gazebo6-dev
+sudo apt-get update
+sudo apt-get install gz-garden -y
--- run_dev.sh
+++ run_dev.sh
@@ -3,3 +3,4 @@ export HOST_UID=$(id -u)
 export HOST_GID=$(id -g)
 xhost +local:docker
 docker compose up
+
